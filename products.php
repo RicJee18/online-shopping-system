be9287b7f8ec596b0e56b1d91282340342090e36
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-05-06 20:24:02 +0800

    added

--- products.php
+++ products.php
@@ -64,12 +64,359 @@
         </div>
       </div>
     </div>
-
-    <div class="services">
-      <div class="container">
-        
-      </div>
+    
+    <div class="section-heading mt-5">
+      <h2>VIVO <br><em>"Brave New World"</em></h2>
+      <span>"Camera & Music"</span>
     </div>
+     <div class="col-md-12 mt-2">
+        <div class="card-deck">
+          <div class="card mb-4">
+            <img src="product-images/vivo-img/Y17 Pro 21999.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >VIVO Y17 Pro</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>25,000.00 </del> &nbsp; <sup>Php</sup> 21,999.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/vivo-img/Y91  32GB 5999.jpg" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >VIVO Y91 32GB</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>7,000.00 </del> &nbsp; <sup>Php</sup> 5,999.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/vivo-img/Y11 5999.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >VIVO Y11</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>6,000.00 </del> &nbsp; <sup>Php</sup> 5,999.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/vivo-img/Y15 12999.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >VIVO Y15</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>15,000.00 </del> &nbsp; <sup>Php</sup> 12,999.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/vivo-img/Y81 7999.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >VIVO Y81</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>9,000 </del> &nbsp; <sup>Php</sup> 7,999</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="section-heading mt-5">
+      <h2>REALME <br><em>"Dare to Leap"</em></h2>
+      <span>"Proud to be Young"</span>
+    </div>
+     <div class="col-md-12 mt-2">
+        <div class="card-deck">
+          <div class="card mb-4">
+            <img src="product-images/realme-img/realme narzo 20 7990.jpg" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >realme narzo 20</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>10,000.00 </del> &nbsp; <sup>Php</sup> 7,990.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/realme-img/realme X3 SuperZoom 22990.jpg" class="mt-2 mb-2">
+            <h6 class="card-title mb-3 text-center" style="font-size:20px;" >realme X3 SuperZoom</h6>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>25,000 </del> &nbsp; <sup>Php</sup> 22,990.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/realme-img/realme C15 6490.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >realme C15</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>7,000.00 </del> &nbsp; <sup>Php</sup> 6,490.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/realme-img/realme 7Pro 15990.jpg" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >realme 7Pro</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>17,000.00 </del> &nbsp; <sup>Php</sup> 15,990.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/realme-img/realme C25 7490.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >realme C25</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>9,000.00 </del> &nbsp; <sup>Php</sup> 7,490.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="section-heading mt-5">
+      <h2>OPPO <br><em>"Flaunt Your Night"</em></h2>
+      <span>"Designed for Life"</span>
+    </div>
+     <div class="col-md-12 mt-2">
+        <div class="card-deck">
+          <div class="card mb-4">
+            <img src="product-images/oppo-img/OPPO Reno 30,888.00.jpg" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >OPPO Reno</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>32,000.00 </del> &nbsp; <sup>Php</sup> 30,888.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/oppo-img/OPPO F9 5499.00.jpg" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >OPPO F9</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>7,000.00 </del> &nbsp; <sup>Php</sup> 5,499.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/oppo-img/OPPO F7 18,999.00.jpg" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >OPPO F7</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>20,000.00 </del> &nbsp; <sup>Php</sup> 18,999.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/oppo-img/OPPO A5s 2039.00.jpg" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >OPPO A5s</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>5,000.00 </del> &nbsp; <sup>Php</sup> 2,030.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/oppo-img/OPPO F5 3080.00.jpg" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >OPPO F5</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>5,000 </del> &nbsp; <sup>Php</sup> 3,080</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="section-heading mt-5">
+      <h2>SAMSUNG <br><em>"The Next Big Thing"</em></h2>
+      <span>"Inspire the World, Create the future"</span>
+    </div>
+     <div class="col-md-12 mt-2">
+        <div class="card-deck">
+          <div class="card mb-4">
+            <img src="product-images/samsung-img/Galaxy Note20 48990.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >SAMSUNG Galaxy Note20</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>50,000.00 </del> &nbsp; <sup>Php</sup> 48,990.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/samsung-img/Galaxy Z Fold2 99990.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >SAMSUNG Galaxy Z Fold2</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>102,000.00 </del> &nbsp; <sup>Php</sup> 99,990.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/samsung-img/Galaxy A72 23990.png" class="mt-2 mb-2" style="height:235px;">
+            <h4 class="card-title mb-3 text-center" >SAMSUNG Galaxy A72</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>25,000.00 </del> &nbsp; <sup>Php</sup> 23,990.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/samsung-img/Galaxy S21 Ultra 5G 69990.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >SAMSUNG Galaxy S21 Ultra 5G</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup>72,000.00 </del> &nbsp; <sup>Php</sup> 69,990.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+          <div class="card mb-4">
+            <img src="product-images/samsung-img/Galaxy S21+ 5G 57990.png" class="mt-2 mb-2">
+            <h4 class="card-title mb-3 text-center" >SAMSUNG Galaxy S21+ 5G</h4>
+            <div style="margin-bottom:10px;" class="text-center">
+              <span><del><sup>Php</sup> 60,000.00 </del> &nbsp; <sup>Php</sup> 57,990.00</span>
+            </div>
+            <div class='product-rating mx-auto  text-center'>
+              <em>RATING</em>&nbsp;&nbsp;
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+            </div>
+            <button type="submit" id="cart-btn" class="btn mb-4 btn-primary w-50 mx-auto text-center mt-1"><a href="login.php">BUY NOW</a></button>
+          </div>
+        </div>
+      </div>
 
     <!-- Footer-->
     <?php include_once('./inc/footer.php'); ?>
